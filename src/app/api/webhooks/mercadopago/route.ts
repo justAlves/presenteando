50faf31f19237beae209ddb71dff49af74b0da93
Guilherme Alves Profirio <guilherme.alves@devvo.com.br>
2025-01-06 00:46:18 -0300

    feat: webhook status teste

--- src/app/api/webhooks/mercadopago/route.ts
+++ src/app/api/webhooks/mercadopago/route.ts
@@ -1,5 +1,14 @@
+import payment from "@/services/mercadopagoClient"
+
 export async function POST(req: Request){
-  console.log('req', req)
+  const params = new URLSearchParams(req.url)
+
+  const id = params.get('data.id')
+
+  const paymentStatus = payment.get({ id: id as string})
+
+  console.log(paymentStatus)
+
 
   return Response.json({
     status: 200,
